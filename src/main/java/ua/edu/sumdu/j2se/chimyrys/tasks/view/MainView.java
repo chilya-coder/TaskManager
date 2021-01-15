20650cb15442ed1763f4bcf4b5399711db755c3e
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-15 14:21:26 +0200

    MainView.java was modified and provided with javadoc

--- src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/view/MainView.java
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/view/MainView.java
@@ -6,14 +6,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainView {
+    /**
+     * Class that provides user with printing main menu and gets user's choice
+     * via this menu.
+     */
     public void printMainMenu() {
         System.out.println("1. Show all tasks");
         System.out.println("2. Show calendar");
-        System.out.println("3. Add/Update task");
-        System.out.println("4. Delete task");
-        System.out.println("5. Show info about task");
-        System.out.println("6. Quit");
+        System.out.println("3. Add/update/delete task");
+        System.out.println("4. Show info about task");
+        System.out.println("5. Quit");
     }
+
+    /**
+     * Method that gets user's choice as int and checks if
+     * there is a such value in UserChoice enum
+     * @return UserChoice
+     */
     public UserChoice getUserChoice() {
         System.out.print("Enter your choice: ");
         Scanner scanner = new Scanner(System.in);
