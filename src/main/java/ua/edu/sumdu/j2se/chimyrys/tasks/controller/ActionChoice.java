e95d3061b06d7c7ce69f5e6fbf9ccc35098cd81c
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-17 19:30:52 +0200

    ActionChoice.java was reformatted

--- src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/controller/ActionChoice.java
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/controller/ActionChoice.java
@@ -1,8 +1,10 @@
 package ua.edu.sumdu.j2se.chimyrys.tasks.controller;
 
 public enum ActionChoice {
-    ADD(1), UPDATE(2), DELETE(3);
-    private int id;
+    ADD(1),
+    UPDATE(2),
+    DELETE(3);
+    protected int id;
     public int getId() {
         return id;
     }
