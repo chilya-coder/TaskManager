18324fc064224fe4f8841cdda0174551b0471f03
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-17 19:22:13 +0200

    TaskInfoController.java was added

---
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/controller/TaskInfoController.java
@@ -0,0 +1,24 @@
+package ua.edu.sumdu.j2se.chimyrys.tasks.controller;
+
+import ua.edu.sumdu.j2se.chimyrys.tasks.model.AbstractTaskList;
+import ua.edu.sumdu.j2se.chimyrys.tasks.view.ModifyTasksView;
+import ua.edu.sumdu.j2se.chimyrys.tasks.view.ShowTaskView;
+
+public class TaskInfoController extends Controller{
+    private ShowTaskView view;
+    private ModifyTasksView indexView;
+    public TaskInfoController(AbstractTaskList model) {
+        super();
+        this.model = model;
+        view = new ShowTaskView(model);
+        indexView = new ModifyTasksView();
+    }
+    @Override
+    public void action() {
+        view.printIndexTitleTask();
+        if (model.size() != 0) {
+            view.printTaskByIndex(indexView.getTaskIndex(model));
+        }
+    }
+
+}
