7f85cf54e77dcd7594f648810dfde8eddf77b68a
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-17 19:24:50 +0200

    StringUtils.java was added

---
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/StringUtils.java
@@ -0,0 +1,6 @@
+package ua.edu.sumdu.j2se.chimyrys.tasks;
+
+public class StringUtils {
+    public static final String FILE_PATH = "test.json";
+    public static final String FILE_NOT_FOUND = "Can't find your file!";
+}
