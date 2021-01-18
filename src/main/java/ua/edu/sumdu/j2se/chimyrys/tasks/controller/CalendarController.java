b4860deec4d0ed9fb27ff45c89b3887be02d2332
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-18 20:30:43 +0200

    CalendarController.java and CalendarView.java were modified

--- src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/controller/CalendarController.java
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/controller/CalendarController.java
@@ -1,16 +1,22 @@
 package ua.edu.sumdu.j2se.chimyrys.tasks.controller;
 
 import ua.edu.sumdu.j2se.chimyrys.tasks.model.AbstractTaskList;
+import ua.edu.sumdu.j2se.chimyrys.tasks.model.Tasks;
+import ua.edu.sumdu.j2se.chimyrys.tasks.view.CalendarView;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
-public class CalendarController extends Controller{
-
-    @Override
-    public void action() {
-
-    }
-
+public class CalendarController extends Controller {
+    private CalendarView calendarView;
     public CalendarController(AbstractTaskList model) {
         super(model);
+        calendarView = new CalendarView();
+    }
+    @Override
+    public void action() {
+        List<LocalDateTime> localDateTimes = calendarView.getCalendarTimeFromUser();
+        calendarView.printCalendar(Tasks.calendar(model, localDateTimes.get(0), localDateTimes.get(1)));
     }
 }
--- src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/view/CalendarView.java
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/view/CalendarView.java
@@ -1,4 +1,40 @@
 package ua.edu.sumdu.j2se.chimyrys.tasks.view;
 
+import ua.edu.sumdu.j2se.chimyrys.tasks.model.Task;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.stream.Collectors;
+
 public class CalendarView {
+    private Scanner scanner;
+    public CalendarView() {
+        scanner = new Scanner(System.in);
+    }
+    public List<LocalDateTime> getCalendarTimeFromUser() {
+        List<LocalDateTime> localDateTimes = new ArrayList<>();
+        ModifyTasksView modifyTasksView = new ModifyTasksView();
+        System.out.println("Enter start date for calendar:");
+        LocalDateTime startCalendar = modifyTasksView.getTaskTime();
+        System.out.println("Enter end date for calendar:");
+        LocalDateTime endCalendar = modifyTasksView.getTaskTime();
+        localDateTimes.add(startCalendar);
+        localDateTimes.add(endCalendar);
+        localDateTimes.sort(LocalDateTime::compareTo);
+        return localDateTimes;
+    }
+    public void printCalendar(SortedMap<LocalDateTime, Set<Task>> calendar) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Set<Map.Entry<LocalDateTime, Set<Task>>> entries = calendar.entrySet();
+        for (Map.Entry<LocalDateTime, Set<Task>> entry : entries) {
+            System.out.println(entry.getKey().format(formatter) + " - "
+                    + entry.getValue().stream().map(Task::getTitle).collect(Collectors.joining(", ")));
+        }
+    }
 }
