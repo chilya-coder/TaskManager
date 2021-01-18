67e413c3b14d4aec1c1da60b75f5d8fe764f2c63
Yuliia Chimyrys <yuliya.chimyrys@gmail.com>
2021-01-18 20:31:08 +0200

    StringUtils.java was modified

--- src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/StringUtils.java
+++ src/main/java/ua/edu/sumdu/j2se/chimyrys/tasks/StringUtils.java
@@ -3,4 +3,7 @@ package ua.edu.sumdu.j2se.chimyrys.tasks;
 public class StringUtils {
     public static final String FILE_PATH = "test.json";
     public static final String FILE_NOT_FOUND = "Can't find your file!";
+    public static final String INVALID_DATE_INPUT = "Note: the date should "
+            + "be bigger than current if you are adding new task to system ";
+    public static final String INVALID_INPUT_VALUE = "You entered invalid value.";
 }
